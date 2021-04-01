e446a3fa47435fde598c18e6aaff560d681b6e27
sysadmin <sysadmin@bcalab1-m21>
2021-04-01 10:03:21 +0530

    thermos

--- src/project1/Thermos.java
+++ src/project1/Thermos.java
@@ -1,5 +1,28 @@
 package project1;
 
 public class Thermos {
+	float qty;
+	String brand;
+	String liquid;
+	
+	Thermos(String liquid,double qty,String brand)
+	{
+		this.liquid =liquid;
+		this.qty=qty;
+		this.brand=brand;
+	}
+	void add(double val)
+	{
+		qty = qty = val;
+	}
+	void display()
+	{
+		System.out.println("Liquid is "+liquid);
+	}
+	
+	public static void main(String args[]) {
+		
+	}
+	
 
 }
